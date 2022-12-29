cd97d418752d057c5cfe6e203913df3164262d67
luke <luke@lukeholten.com>
2022-12-29 23:38:55 +0000

    Optimised the staff searchbar, users may now use arrow keys and press enter to search

--- pages/php/autocomplete/autocomplete_staff_searchbar.php
+++ pages/php/autocomplete/autocomplete_staff_searchbar.php
@@ -1,32 +1,110 @@
 <?php
-// SQL query used to change the location popularity stored on the table
-$query = "SELECT Users.Forename, Users.Surname, Users.UserID FROM `Users` WHERE CONCAT(Users.Forename, ' ', Users.Surname) LIKE ? LIMIT 0, 5";
-// Connects to the database
-$con = new mysqli('localhost', 'dreg_user', 'epq', 'dregDB');
-// turns the query into a statement
-$stmt = $con->prepare($query);
+// Used to search through all menu items
+function searchMenu($term) {
+  // Array of all applicable menu
+  $menuIndex = [
+    ["home", 'home_button', 'home_section'],
+    ["activity", 'activity_button', 'activity_section'],
+    ["users", 'users_button', 'users_section'],
+    ["restricted", 'restricted_button', 'restricted_section'],
+    ["away", "away_button", "away_section"],
+    ["summary", "events_summary_button", "events_summary_section"],
+    ["events", "events_summary_button", "events_summary_section"],
+    ["edit details", "edit_details_button", "edit_details_section"],
+    ["customise", "customise_button", "customise_section"]
+  ];
+  // A string is used to contain all 'suggestion' list elements
+  $suggestions = "";
+  // For each menu item, check whether the search term is similar
+  for ($i = 0; $i < count($menuIndex); $i++) {
+    // If the term is similar to the current menu item, suggest the menu item
+    if (stripos($menuIndex[$i][0], $term) !== FALSE) {
+      // Function called when the suggested item is clicked
+      $function = "toggleMainSection('".$menuIndex[$i][1]."', '".$menuIndex[$i][2]."')";
+      // The HTML used to display the list item
+      $suggestions .= htmlspecialchars('<li onclick="'.$function.'">'.ucwords($menuIndex[$i][0]).'</li>');
+    }
+  }
+  // Return the suggestions
+  return $suggestions;
+}
+// Used to search through all the events
+function searchEvents($term) {
+  // SQL to search the database for users
+  $query = "SELECT LOWER(REPLACE(Event, ' ', '_')), Event FROM `Events` WHERE Event LIKE CONCAT('%', ?, '%') LIMIT 0, 5";
+  // Connects to the database
+  $con = new mysqli('localhost', 'dreg_user', 'epq', 'dregDB');
+  // turns the query into a statement
+  $stmt = $con->prepare($query);
+  // Inserts the term into the query
+  $stmt->bind_param("s", $term);
+  // Executes the statement code
+  $stmt->execute();
+  $result = $stmt->get_result();
+  // Disconnects from the database
+  $con->close();
+
+  // A string is used to contain all 'suggestion' list elements
+  $suggestions = "";
+  // Iterates through the results and adds each user to a variable
+  while($record = $result -> fetch_array(MYSQLI_NUM)) {
+    // Determines the section id for this suggestion
+    $section_id = strtolower("$record[0]_event_section");
+    // Determines the button id for this suggestion
+    $button_id = strtolower("$record[0]_event_button");
+    // Function called when the suggested item is clicked
+    $function = "toggleMainSection('".$button_id."', '".$section_id."')";
+    // Creates the HTML required for the suggestion
+    $suggestions .= htmlspecialchars('<li onclick="'.$function.'">'.ucwords($record[1]).'</li>');
+  }
+  // Return the suggestions
+  return $suggestions;
+}
+// Used to search for all users
+function searchUsers($term) {
+  // SQL to search the database for users
+  $query = "SELECT Users.Forename, Users.Surname, Users.UserID FROM `Users` WHERE CONCAT(Users.Forename, ' ', Users.Surname) LIKE CONCAT('%', ?, '%') ORDER BY Users.Forename LIMIT 0, 5";
+  // Connects to the database
+  $con = new mysqli('localhost', 'dreg_user', 'epq', 'dregDB');
+  // turns the query into a statement
+  $stmt = $con->prepare($query);
+  // Inserts the term into the query
+  $stmt->bind_param("s", $term);
+  // Executes the statement code
+  $stmt->execute();
+  $result = $stmt->get_result();
+  // Disconnects from the database
+  $con->close();
+
+  // A string is used to contain all 'suggestion' list elements
+  $suggestions = "";
+  // Iterates through the results and adds each user to a variable
+  while($record = $result -> fetch_array(MYSQLI_NUM)) {
+    $suggestions .= htmlspecialchars("<li onclick='displayUserDetails($record[2])'>$record[0] $record[1]</li>");
+  }
+  // Return the suggestions
+  return $suggestions;
+}
+
+// Combines all the search functions into one
+function search($term) {
+  // Note that this is a long string containing every suggestion as a list item element (<li>)
+  $searchResults = "";
+  // The values are displayed in order of the functions where the top functions' results takes priority
+  $searchResults .= searchMenu($term);
+  $searchResults .= searchEvents($term);
+  $searchResults .= searchUsers($term);
+  // The returned value is encoded using 'htmlspecialchars()' so it will need decoding
+  return $searchResults;
+}
 
 // If the term is not empty
 if ($_POST['term'] != "") {
-  // Concatenates a '%' to the start and end of the term to make the SQL query more effective
-  $term = "%".$_POST['term']."%";
+  // Perform a search and output the result
+  echo htmlspecialchars_decode(search($_POST['term']));
 }
-// If the term is empty, return none
+// If the term is empty output no results
 else {
   echo "";
-}
-
-// Inserts the term into the query
-$stmt->bind_param("s", $term);
-// Executes the statement code
-$stmt->execute();
-$result = $stmt->get_result();
-// Disconnects from the database
-$con->close();
-
-
-// Iterates through the table records and displays them on the web page's table
-while($record = $result -> fetch_array(MYSQLI_NUM)) {
-  echo "<li onclick='displayUserDetails($record[2])'>$record[0] $record[1]</li>";
 }
 ?>
--- scripts/jquery/staff/searchbar_autocomplete.js
+++ scripts/jquery/staff/searchbar_autocomplete.js
@@ -1,30 +1,107 @@
-// Used to display suggested text under the staff search bar
+// Show suggestions
+function showSuggestions() {
+  $('#staff_searchbar_suggestions').css('display','block');
+}
+// Hide suggestions
+function hideSuggestions() {
+  $('#staff_searchbar_suggestions').css('display','none');
+}
+
 $(document).ready(function() {
-  // Detects when text is input into the searchbar
-  $("#staff_searchbar").keyup(function() {
-    $.ajax({
-        type: 'POST',
-        url: 'autocomplete/autocomplete_staff_searchbar.php',
-        data: { term: $("#staff_searchbar").val() },
-        beforeSend: function() {
-          // Clears the search bar if it is empty
-          if ($("#staff_searchbar").val() === "") {
-            $('#staff_searchbar_suggestions').empty();
-          }
-        },
-        success: function(data) {
-          $('#staff_searchbar_suggestions').empty();
-          $('#staff_searchbar_suggestions').append(data);
+  // Variables
+  // Determines whether suggestions are hidden
+  hidden = false;
+  // The number of suggestions available (0 by default)
+  suggestionCount = 0;
+  // Gives the index for the suggestion that is currently selected using the arrow keys
+  selectedSuggestion = 1;
+
+  // When the enter key is pressed anywhere on the page and the user is not already using an input, focus on the search bar
+  $(document).on('keypress',function(e) {
+      if(e.which == 13 && !($("input").is(":focus"))) {
+          $('#staff_searchbar').focus();
       }
-    });
   });
-  // Clears the searchbar when it is not selected
-  document.getElementById('staff_searchbar').addEventListener('focusout', function() {
+
+  // Functions for the various searchbar events
+  $('#staff_searchbar').on({
+    // If the arrow keys are pressed when the user is searching (top priority as they determine which suggestion the enter key selects)
+    keydown : function (e) {
+      if(e.which == 38 && selectedSuggestion > 1) {
+        selectedSuggestion -= 1;
+      }
+      // Down arrow key (select the suggestion below the current one)
+      if(e.which == 40 && selectedSuggestion < suggestionCount) {
+        selectedSuggestion += 1;
+      }
+    },
+    // If the enter key is pressed when the user is searching (this is prioritized over the keyup event)
+    keypress : function (e) {
+      // Enter key (search for top suggestion)
+      if(e.which == 13) {
+        // Simulate a 'click event' on the selected suggestion (top suggestion by default) and resets the selected suggestion
+        $('#staff_searchbar_suggestions li:nth-of-type('+selectedSuggestion+')').trigger('click');
+        selectedSuggestion = 1;
+      }
+    },
+    // If the user inputs any data into the search bar, a search is performed and suggestions are added and displayed
+    keyup : function () {
+      // Uses Ajax to post the search term to the server and retrieve suggestions
+      $.ajax({
+          type: 'POST',
+          url: 'autocomplete/autocomplete_staff_searchbar.php',
+          data: { term: $('#staff_searchbar').val() },
+          beforeSend: function() {
+            // Removes all suggestions if the staff searchbar is empty
+            if ($('#staff_searchbar').val() === '') {
+              $('#staff_searchbar_suggestions').empty();
+            }
+          },
+          success: function(data) {
+            // When the suggestions are recieved, removes previous, appends new ones and displays them
+            $('#staff_searchbar_suggestions').empty();
+            $('#staff_searchbar_suggestions').append(data);
+            $('#staff_searchbar_suggestions').css('display','block');
+            // The total number of suggestions available
+            suggestionCount = $("#staff_searchbar_suggestions li").length;
+            // If the selected section is a value greater than the number of suggestions, it is reset
+            if (selectedSuggestion > suggestionCount) {
+              selectedSuggestion = 1;
+            }
+            // Sets the background color of the selected suggestion
+            $('#staff_searchbar_suggestions:not(:hover) li:nth-of-type('+selectedSuggestion+')').css('background', 'rgba(198, 196, 200, 0.9)');
+        }
+      });
+    },
+    // If the user clicks on the search bar, it shows the current suggestions (rather than performing another search)
+    focus: function () {
+      // Shows the suggestions
+      $('#staff_searchbar_suggestions').css('display','block');
+    },
+    // If the user clicks away from the search bar, it hides the current suggestions
+    focusout: function () {
+      // Hides the suggestions after 1000ms
       setTimeout(function() {
-        // Clear all text from searchbar
-        $('#staff_searchbar').val('');
-        // Remove all list items from the suggested inputs
-        $('#staff_searchbar_suggestions').empty();
-      }, 200);
-    });
+        // If the suggestions are not hidden, hide them
+        // (this is to prevent the timeout from carrying over until something else is searched, hidding the suggestions mid-search)
+        if (!hidden) {
+          $('#staff_searchbar_suggestions').css('display','none');
+        }
+      }, 1000);
+    }
+  });
+
+  // Functions performed on the searchbar suggestions
+  $('#staff_searchbar_suggestions').on({
+    // When a suggestion is selected
+    click : function() {
+      // Hides the suggestions
+      $('#staff_searchbar_suggestions').css('display','none');
+      hidden=true
+      // Removes all suggestions (to prevent future searches containing old search results)
+      $('#staff_searchbar_suggestions').empty();
+      // Clears the search bar of all text
+      $('#staff_searchbar').val('');
+    }
+  });
 });
